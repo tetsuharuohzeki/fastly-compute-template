9cef9149e59491b7078c2ba7ef076d185867f566
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2022-03-25 23:30:19 +0900

    Fix `cargo clippy` warnings

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use fastly::{Error, Request, Response};
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
-    return c_at_e_main::application_main(req);
+    c_at_e_main::application_main(req)
 }
