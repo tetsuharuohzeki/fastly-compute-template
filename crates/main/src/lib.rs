f67ae5dd270ded4fd2b3f89ed39df2d1def0d3e9
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2022-03-25 17:29:32 +0900

    Move the primary implementations to `main` crate to organize codes.
    fastly CLI (at least v1.7.1) does not support a way to work with Cargo Workspace.
    This mean that we need to keep a crate as an entry point on the directory
    which we run `fastly compute build`, `deploy`, etc. because
    fastly CLI check & use `package.name` field in Cargo.toml in the current working directory
    to indentify the build artifact (wasm file) which fastly CLI manipulate before deploying it.

--- crates/main/src/lib.rs
+++ crates/main/src/lib.rs
@@ -1,1 +1,6 @@
+use fastly::http::StatusCode;
+use fastly::{Error, Request, Response};
 
+pub fn application_main(_req: Request) -> Result<Response, Error> {
+    Ok(Response::from_status(StatusCode::OK))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use fastly::http::StatusCode;
 use fastly::{Error, Request, Response};
 
 #[fastly::main]
-fn main(_req: Request) -> Result<Response, Error> {
-    Ok(Response::from_status(StatusCode::OK))
+fn main(req: Request) -> Result<Response, Error> {
+    return c_at_e_main::application_main(req);
 }
