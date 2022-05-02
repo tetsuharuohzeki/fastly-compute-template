aed16b4ec23bf0978430abc6938a043907fd5347
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2022-05-03 03:12:26 +0900

    Add more mime types

--- integration_tests/__helpers__/mime.js
+++ integration_tests/__helpers__/mime.js
@@ -1,1 +1,39 @@
+// Sort with https://github.com/hyperium/mime
+
+export const STAR_STAR = '*/*';
+
+export const TEXT_STAR = 'text/*';
+export const TEXT_PLAIN = 'text/plain';
 export const TEXT_PLAIN_UTF_8 = 'text/plain; charset=utf-8';
+export const TEXT_HTML = 'text/html';
+export const TEXT_HTML_UTF_8 = 'text/html; charset=utf-8';
+export const TEXT_CSS = 'text/css';
+export const TEXT_CSS_UTF_8 = 'text/css; charset=utf-8';
+export const TEXT_JAVASCRIPT = 'text/javascript';
+export const TEXT_XML = 'text/xml';
+export const TEXT_EVENT_STREAM = 'text/event-stream';
+export const TEXT_CSV = 'text/csv';
+export const TEXT_CSV_UTF_8 = 'text/csv; charset=utf-8';
+export const TEXT_TAB_SEPARATED_VALUES = 'text/tab-separated-values';
+export const TEXT_TAB_SEPARATED_VALUES_UTF_8 = 'text/tab-separated-values; charset=utf-8';
+export const TEXT_VCARD = 'text/vcard';
+
+export const IMAGE_STAR = 'image/*';
+export const IMAGE_JPEG = 'image/jpeg';
+export const IMAGE_GIF = 'image/gif';
+export const IMAGE_PNG = 'image/png';
+export const IMAGE_BMP = 'image/bmp';
+export const IMAGE_SVG = 'image/svg+xml';
+
+export const FONT_WOFF = 'font/woff';
+export const FONT_WOFF2 = 'font/woff2';
+
+export const APPLICATION_JSON = 'application/json';
+export const APPLICATION_JAVASCRIPT = 'application/javascript';
+export const APPLICATION_JAVASCRIPT_UTF_8 = 'application/javascript; charset=utf-8';
+export const APPLICATION_WWW_FORM_URLENCODED = 'application/x-www-form-urlencoded';
+export const APPLICATION_OCTET_STREAM = 'application/octet-stream';
+export const APPLICATION_MSGPACK = 'application/msgpack';
+export const APPLICATION_PDF = 'application/pdf';
+
+export const MULTIPART_FORM_DATA = 'multipart/form-data';
