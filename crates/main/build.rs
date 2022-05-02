09789da84621124a93203dd3d3bc00a05b6f4ef5
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2022-05-02 22:42:48 +0900

    Don't panic even if there are not `git` or `date` command on compiling time.

--- crates/main/build.rs
+++ crates/main/build.rs
@@ -1,20 +1,23 @@
+use std::error::Error;
+use std::ffi::OsStr;
 use std::process::Command;
 
 fn main() {
-    let git_output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
-        .output()
-        .expect("could get git commit hash");
-
-    let git_hash = String::from_utf8(git_output.stdout)
+    let git_hash = get_command_stdout_str("git", ["rev-parse", "HEAD"])
         .unwrap_or_else(|_| "[could not get git-commit-hash]".to_string());
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 
-    let date_output = Command::new("date")
-        .args(&["+%Y/%m/%d %H:%M:%S %z"])
-        .output()
-        .expect("could not get date");
-    let build_date = String::from_utf8(date_output.stdout)
+    let build_date = get_command_stdout_str("date", ["+%Y/%m/%d %H:%M:%S %z"])
         .unwrap_or_else(|_| "[could not built-date]".to_string());
     println!("cargo:rustc-env=BUILD_DATE={}", build_date);
+}
+
+fn get_command_stdout_str<S, I>(program: S, args: I) -> Result<String, Box<dyn Error>>
+where
+    S: AsRef<OsStr>,
+    I: IntoIterator<Item = S>,
+{
+    let output = Command::new(program).args(args).output()?;
+    let string = String::from_utf8(output.stdout)?;
+    Ok(string)
 }
