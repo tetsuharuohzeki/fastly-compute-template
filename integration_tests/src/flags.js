1f9ce29372ca980770a2aeaa6ca436945f97331c
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2022-05-02 16:13:53 +0900

    [integration_tests] Set flags as boolean correctly

--- integration_tests/src/flags.js
+++ integration_tests/src/flags.js
@@ -1,11 +1,17 @@
 import { expectNotUndefined } from 'option-t/esm/Undefinable/expect';
-import { unwrapOrFromUndefinable } from 'option-t/esm/Undefinable/unwrapOr';
+import { mapOrForUndefinable } from 'option-t/esm/Undefinable/mapOr';
+
+function isTrue(value) {
+    const ok = value === 'true';
+    return ok;
+}
 
 export const RELEASE_CHANNEL = expectNotUndefined(process.env.RELEASE_CHANNEL, 'RELEASE_CHANNEL envvar must be set');
 
-export const UPDATE_SNAPSHOTS = unwrapOrFromUndefinable(process.env.UPDATE_SNAPSHOTS, false);
+export const UPDATE_SNAPSHOTS = mapOrForUndefinable(process.env.UPDATE_SNAPSHOTS, false, isTrue);
 
-export const LAUNCH_INTEGRATION_TEST_FORMATION = unwrapOrFromUndefinable(
+export const LAUNCH_INTEGRATION_TEST_FORMATION = mapOrForUndefinable(
     process.env.LAUNCH_INTEGRATION_TEST_FORMATION,
-    false
+    false,
+    isTrue
 );
