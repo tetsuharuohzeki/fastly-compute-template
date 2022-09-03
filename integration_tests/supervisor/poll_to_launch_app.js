6a45d28dad551dee84ab63512292d9fe54112376
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2022-09-04 02:27:38 +0900

    Include the initial wait time to the deadline to launch the app

--- integration_tests/supervisor/poll_to_launch_app.js
+++ integration_tests/supervisor/poll_to_launch_app.js
@@ -3,23 +3,28 @@ import { setMaxListeners } from 'node:events';
 import { setTimeout } from 'node:timers/promises';
 import { fetch } from 'undici';
 
+// This value is just heuristics.
+// We always need to await about 1~ sec to launch a new process for the application.
+// For example, in my machine (MBP15 2017), the release build requires 1.5 sec to launche the app.
+const INITIAL_WAIT_MS = 1500;
+
 const POLLING_INTERVAL_MS = 100;
 
 export async function pollToLaunchApplication(signal, url, timeoutMillisec) {
     assert.ok(signal instanceof AbortSignal);
+    assert.ok(typeof url === 'string');
+    assert.ok(typeof timeoutMillisec === 'number');
+    assert.ok(timeoutMillisec > INITIAL_WAIT_MS, `timeoutMillisec must be >= ${INITIAL_WAIT_MS}ms`);
 
-    // This value is just heuristics.
-    // For example, in my machine (MBP15 2017), the release build requires 1.5 sec to launche the app.
-    // So we always need to await 1 sec for the application.
-    await setTimeout(1500);
-
-    // This avoid the waring:
+   // This avoid the waring:
     //  > (node:12052) MaxListenersExceededWarning: Possible EventTarget memory leak detected.
     //  > 11 abort listeners added to [AbortSignal]. Use events.setMaxListeners() to increase limit
     setMaxListeners(100, signal);
 
+    await setTimeout(INITIAL_WAIT_MS);
+
     const begin = Date.now();
-    const deadline = begin + timeoutMillisec;
+    const deadline = begin + (timeoutMillisec - INITIAL_WAIT_MS);
 
     while (!signal.aborted && Date.now() <= deadline) {
         try {
