c505ac65b13d1284a20e59df1cd59f087dfa2764
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2022-09-27 08:54:42 +0900

    Add the compile-time check that tests release channel is mutually exclusive.
    see https://internals.rust-lang.org/t/mutually-exclusive-feature-flags/8601/4

--- crates/main/src/main.rs
+++ crates/main/src/main.rs
@@ -7,3 +7,19 @@ use fastly::{Error, Request, Response};
 fn main(req: Request) -> Result<Response, Error> {
     application::main(req)
 }
+
+#[cfg(all(not(feature = "production"), not(feature = "canary")))]
+compile_error!("You must specify the release channel.");
+
+#[cfg(not(any(feature = "production", feature = "canary")))]
+compile_error!("You must choose any one of release channels.");
+
+// The current cargo does not have a way to define mutually exclusive features.
+// https://github.com/rust-lang/cargo/issues/2980
+//
+// We need to do same compile-time check by the combination of `#[cfg]`.
+#[cfg(any(
+    all(feature = "production", any(feature = "canary")),
+    all(feature = "canary", any(feature = "production")),
+))]
+compile_error!("Release channel must be exclusively for each other.");
