265f5da5109bbc536e9cc63414350414fa386a82
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-07-14 23:52:30 +0900

    Specify mock server's port via cli flag

--- integration_tests/mock_server/main.js
+++ integration_tests/mock_server/main.js
@@ -3,16 +3,17 @@ import * as HttpStatus from '../http_helpers/http_status.js';
 import * as Mime from '../http_helpers/mime.js';
 
 import { createHttpServer } from './shared/create_server.js';
+import { parseCliOptions } from './shared/parse_cli.js';
 
-const PORT = 8030;
+const { port } = parseCliOptions();
 
-createHttpServer('', PORT, async (_req, res, url) => {
+createHttpServer('mock_main', port, async (_req, res, url) => {
     const urlPathname = url.pathname;
 
     if (urlPathname === '/hello_this_is_mock') {
         res.setHeader(HttpHeader.CONTENT_TYPE, Mime.TEXT_PLAIN_UTF_8);
         res.writeHead(HttpStatus.OK);
-        res.end(`this is mock server listening on ${String(PORT)}`);
+        res.end(`this is mock server listening on ${String(port)}`);
         return true;
     }
 
---
+++ integration_tests/mock_server/shared/parse_cli.js
@@ -0,0 +1,23 @@
+import * as assert from 'node:assert/strict';
+import { parseArgs } from 'node:util';
+
+export function parseCliOptions() {
+    const { values } = parseArgs({
+        options: {
+            port: {
+                type: 'string',
+                default: '',
+            },
+        },
+        strict: true,
+    });
+
+    const portString = values.port;
+    assert.notStrictEqual(portString, '', `must specify --port cli flag`);
+
+    const port = +portString;
+
+    return {
+        port,
+    };
+}
