8655d602966c8c8124439a8d3a4e320a94b7c7e5
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2023-09-18 22:43:41 +0900

    Add the status code check for integration_tests/__tests__/bypass_to_mock.test.js

--- integration_tests/__tests__/bypass_to_mock.test.js
+++ integration_tests/__tests__/bypass_to_mock.test.js
@@ -2,6 +2,7 @@ import test from 'ava';
 import { fetch } from 'undici';
 
 import * as HttpHeader from '../http_helpers/http_header.js';
+import * as HttpStatus from '../http_helpers/http_status.js';
 import * as Mime from '../http_helpers/mime.js';
 
 import { constructUrl } from './__helpers__/domain.js';
@@ -14,6 +15,8 @@ const URL = constructUrl(PATH);
 
 test(PATH, async (t) => {
     const res = await fetch(URL);
+    t.is(res.status, HttpStatus.OK);
+
     const headers = res.headers;
     t.is(headers.get(CONTENT_TYPE_HEADER), MIME_TEXT_PLAIN_UTF_8, `${CONTENT_TYPE_HEADER} header value`);
 
