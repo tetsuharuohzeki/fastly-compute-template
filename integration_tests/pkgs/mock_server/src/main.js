4fc0c98d896439a8e286a43e8475e569dbff4c0d
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2025-06-28 22:57:19 +0900

    Revert "integration_tests/supervisor: Add example for mock server router based by URLPattern"
    This reverts commit 0342f5bfd48720dbf0d467c8fd62ffb66d9b3b49 due to CI
    failures:
    
    ```
    file:////fastly-compute-template/integration_tests/pkgs/mock_server/src/shared/mini_url_pattern_router.js:43
            const pattern = new URLPattern(pat);
                            ^
    
    TypeError: Failed to construct URLPattern
        at file:///fastly-compute-template/integration_tests/pkgs/mock_server/src/shared/mini_url_pattern_router.js:43:25
        at Array.map (<anonymous>)
        at buildMatcherFromURLPattern (file:////integration_tests/pkgs/mock_server/src/shared/mini_url_pattern_router.js:42:38)
        at file:////integration_tests/pkgs/mock_server/src/main.js:9:17
        at ModuleJob.run (node:internal/modules/esm/module_job:365:25)
        at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:665:26)
        at async asyncRunEntryPointWithESMLoader (node:internal/modules/run_main:99:5) {
      code: 'ERR_INVALID_URL_PATTERN'
    }
    ```

--- integration_tests/pkgs/mock_server/src/main.js
+++ integration_tests/pkgs/mock_server/src/main.js
@@ -2,28 +2,12 @@ import { HttpHeader, HttpStatus, Mime } from '@c_at_e_integration_tests/http_hel
 
 import { createHttpServer } from './shared/create_server.js';
 import { parseCliOptions } from './shared/parse_cli.js';
-import { buildMatcherFromURLPattern, tryMatchAndCallRouteHandler } from './shared/mini_url_pattern_router.js';
 
 const { port } = parseCliOptions();
 
-const matcher = buildMatcherFromURLPattern([
-    [
-        `/hello_this_is_mock_by_url_pattern`,
-        async (_req, res, _pattern, _url) => {
-            res.setHeader(HttpHeader.CONTENT_TYPE, Mime.TEXT_PLAIN_UTF_8);
-            res.writeHead(HttpStatus.OK);
-            res.end(`this is mock server listening on ${String(port)}`);
-        },
-    ],
-]);
-
 createHttpServer('mock_main', port, async (_req, res, url) => {
-    const isHandled = await tryMatchAndCallRouteHandler(matcher, _req, res, url);
-    if (isHandled) {
-        return true;
-    }
-
     const urlPathname = url.pathname;
+
     switch (urlPathname) {
         case '/hello_this_is_mock': {
             res.setHeader(HttpHeader.CONTENT_TYPE, Mime.TEXT_PLAIN_UTF_8);
